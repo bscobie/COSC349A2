b0830dd4bc85fd93c894259f6551c4983fca6f8d
bscobie <86281691+bscobie@users.noreply.github.com>
2022-10-08 11:02:40 +1300

    Set up db query, now retreives bookings from RDS.

--- adminserver/www/html/index.php
+++ adminserver/www/html/index.php
@@ -12,16 +12,17 @@
 <table>
 <tr><th>Booking No.</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Game Title</th><th>Rental Length</th></tr>
 <?php
+  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-$q = $pdo->query("SELECT bookings.bookingID, bookings.fname, bookings.lname, bookings.email, bookings.length, games.title FROM bookings INNER JOIN games ON bookings.gameID=games.gameID");
+  $database = mysqli_select_db($connection, DB_DATABASE);
 
-while($row = $q->fetch()){
-  echo "<tr><td>".$row["bookingID"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["email"]."</td>
-  <td>".$row["title"]."</td><td>".$row["length"]."</td></tr>\n";
-}
+  $result = mysqli_query($connection, "SELECT BOOKINGS.bookingID, BOOKINGS.fname, BOOKINGS.lname, BOOKINGS.email, BOOKINGS.length, GAMES.title FROM BOOKINGS INNER JOIN GAMES ON BOOKINGS.gameID=GAMES.gameID");
 
+  while($row = mysqli_fetch_row($result)){
+    echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[5]."</td><td>".$row[4]."</td></tr>\n";
+  }
 ?>
 </table>
 </body>
